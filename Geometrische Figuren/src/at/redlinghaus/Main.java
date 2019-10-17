6aeaf3a2e4aa3654bb5a397f30a3e6070d18c049
Florian Redlinghaus <florian@redlinghaus.at>
2019-10-18 01:20:13 +0200

    update

--- Geometrische Figuren/src/at/redlinghaus/Main.java
+++ Geometrische Figuren/src/at/redlinghaus/Main.java
@@ -4,31 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Printer myPrint = new Printer();
         Point myP1 = new Point(0,0);
-        Point myP2 = new Point(4,5);
+        Point myP2 = new Point(8,4);
         Rectangle myRectang1 = new Rectangle(myP1, myP2);
 
 
-        myPrint.draw(myRectang1);
-        System.out.println("____________________________________________________________");
-        myRectang1.moveR(10, 6);
-        myPrint.draw(myRectang1);
-        System.out.println("____________________________________________________________");
-        myRectang1.rotateRight(1);
-        myPrint.draw(myRectang1);
-        System.out.println("____________________________________________________________");
-        myRectang1.rotateRight(2);
-        myPrint.draw(myRectang1);
-        System.out.println("____________________________________________________________");
-
-
-
-
-//        myPrint.draw(myRectang1.p1);
-//        myPrint.draw(myRectang1.p2);
-//        myPrint.draw(myRectang1.p3);
-//        myPrint.draw(myRectang1.p4);
-
+        Printer.draw(myRectang1);
+        myRectang1.moveR(10, 10);
+        myRectang1.rotateLeft(myP2);
+        myRectang1.rotateLeft(myP2);
+        myRectang1.rotateLeft(myP2);
+        myRectang1.rotateLeft(myP2);
     }
 }
--- Geometrische Figuren/src/at/redlinghaus/Rectangle.java
+++ Geometrische Figuren/src/at/redlinghaus/Rectangle.java
@@ -1,29 +1,20 @@
 package at.redlinghaus;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Rectangle {
-    public int a, b;
+    private int a, b, c, absA, absB;
     private Point m;
-    public Point p1, p2, p3, p4;
-    List<Point> points;
+    private Point p1, p2, p3, p4;
+    private Point[] points = new Point[4];
 
     public Rectangle(Point punkt1, Point punkt3){
         this.p1 = punkt1;
-//        this.p1 = new Point(Math.min(punkt1.getX(), punkt3.getX()), Math.min(punkt1.getY(), punkt3.getY()));
         this.p3 = punkt3;
-//        this.p3 = new Point(Math.max(punkt1.getX(), punkt3.getX()), Math.max(punkt1.getY(), punkt3.getY()));
         this.p2 = new Point(this.p3.getX(), this.p1.getY());
         this.p4 = new Point(this.p1.getX(), this.p3.getY());
-//        this.a = p3.getX() - p1.getX();
-//        this.b = p3.getY() - p1.getY();
-//        this.m = new Point((p3.getX()-p1.getX())/2+p1.getX(),(p3.getY()-p1.getY())/2+p1.getY());
-        this.points = new LinkedList<>();
-        points.add(p1);
-        points.add(p2);
-        points.add(p3);
-        points.add(p4);
+        points[0] = p1;
+        points[1] = p2;
+        points[2] = p3;
+        points[3] = p4;
     }
 
     public void moveR(int xMove, int yMove) {
@@ -35,29 +26,82 @@ public class Rectangle {
         this.p3.setY(this.p3.getY() + yMove);
         this.p4.setX(this.p4.getX() + xMove);
         this.p4.setY(this.p4.getY() + yMove);
+        Printer.draw(this);
     }
 
-    public void rotateRight(int indexAngle) {
-//        if(angle.equals(p1) || drehPunkt.equals(p2) || drehPunkt.equals(p3) || drehPunkt.equals(p4)) {
-        this.p1 = points.get(indexAngle);
-        this.p2 = points.get((indexAngle + 1) % 4);
-        this.p3 = points.get((indexAngle + 2) % 4);
-        this.p4 = points.get((indexAngle + 3) % 4);
+    public void updateSides() {
+        this.absA = Math.abs(p1.getX() - p3.getX());
+        this.absB = Math.abs(p1.getY() - p3.getY());
+        this.a = p1.getX() - p3.getX();
+        this.b = p1.getY() - p3.getY();
+        this.c = (int) Math.sqrt(absA*absA + absB*absB);
+    }
 
-        int a = Math.abs(p1.getX() - p3.getX());
-        int b = Math.abs(p1.getY() - p3.getY());
+    public Point findCounterP (Point angleP) {
+        for(int i = 0; i < 4 ; i++){
+            if (points[i].equals(angleP)) {
+                return points[(i + 2) % 4];
+            }
+        }
+        return null;
+    }
 
-        this.p2.setX(p1.getX());
-        this.p2.setY(p1.getY() - a);
+    public int isPoint(int x, int y) {
+        for (int l = 0; l < 4; l++) {
+            if (this.getPoints()[l].getX() == x && this.getPoints()[l].getY() == y) {
+                return l;
+            }
+        }
+        return -1;
+    }
 
-        this.p3.setX(p1.getX() + a);
-        this.p3.setY(p1.getY() - b);
+    public int getMinX(){
+        return Math.min(p1.getX(), p3.getX());
+    }
 
-        this.p4.setX(p1.getX() + b);
-        this.p4.setY(p1.getY());
-//        } else {
-//            System.out.println("Der Punkt ist kein Eckpunkt des Rechtecks");
-//        }
+    public int getMaxX(){
+        return Math.max(p1.getX(), p3.getX());
+    }
+
+    public int getMinY(){
+        return Math.min(p1.getY(), p3.getY());
+    }
+
+    public int getMaxY(){
+        return Math.max(p1.getY(), p3.getY());
+    }
+
+    public void rotateLeft(Point angleP) {
+        this.p1 = angleP;
+        this.p3 = this.findCounterP(angleP);
+        updateSides();
+
+        if (p3 != null) {
+            this.p3.setX(p1.getX() - b);
+            this.p3.setY(p1.getY() + a);
+
+            if (p1.getY() == p2.getY()) {
+                this.p2.setX(p1.getX());
+                this.p2.setY(p1.getY() + a);
+            } else if (p1.getX() == p2.getX()) {
+                this.p2.setX(p1.getX() - b);
+                this.p2.setY(p1.getY());
+            }
+            if (p1.getY() == p4.getY()) {
+                this.p4.setX(p1.getX());
+                this.p4.setY(p1.getY() + a);
+            } else if (p1.getX() == p4.getX()) {
+                this.p4.setX(p1.getX() - b);
+                this.p4.setY(p1.getY());
+            }
+        } else {
+            System.out.println("Der Punkt ist kein Eckpunkt des Rechtecks");
+        }
+        Printer.draw(this);
+    }
+
+    public Point[] getPoints() {
+        return points;
     }
 
     @Override
