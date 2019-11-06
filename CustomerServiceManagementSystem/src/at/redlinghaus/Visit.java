deb4118ce157a9fcda5aa7dedd26df8cc8446876
Florian Redlinghaus <florian@redlinghaus.at>
2019-11-06 22:12:27 +0100

    Update Visit.java

--- CustomerServiceManagementSystem/src/at/redlinghaus/Visit.java
+++ CustomerServiceManagementSystem/src/at/redlinghaus/Visit.java
@@ -8,18 +8,21 @@ public class Visit {
     private Date date;
     private double totalServiceExpense;
     private double totalProductExpense;
+    private int totalRebate;
 
     public Visit(Visitable location, Customer customer) {
         this.location = location;
         this.customer = customer;
+    }
+
+    public void calcTotalService() {
+        totalServiceExpense = customer.getCart().totalServiceExpense();
+    }
+    public void calcTotalProduct() {
         totalProductExpense = customer.getCart().totalProductExpense();
     }
 
-    public double calcTotalService() {
-        return customer.getCart().totalServiceExpense();
-    }
-
-    public int calcTotalRebate(){
+    public void calcTotalRebate(){
 
     }
 
